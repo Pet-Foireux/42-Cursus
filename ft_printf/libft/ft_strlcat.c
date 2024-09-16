01315dfce1e5810e2cb696bcabe1201061ac4a36
PetFoireux <pet-foireux@oo>
2024-09-16 15:11:38 +0200

    shrek is the best

---
+++ ft_printf/libft/ft_strlcat.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpapin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 16:00:31 by mpapin            #+#    #+#             */
+/*   Updated: 2024/09/10 16:00:31 by mpapin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *d, const char *s, size_t dstsize)
+{
+	size_t	dst_len;
+	size_t	index;
+	size_t	i;
+
+	dst_len = ft_strlen(d);
+	index = 0;
+	while (d[index])
+		index++;
+	i = 0;
+	while (s[i] && (i + index + 1) < (dstsize))
+	{
+		d[index + i] = s[i];
+		i++;
+	}
+	if (i < dstsize)
+		d[index + i] = '\0';
+	if (dstsize <= dst_len)
+		return (ft_strlen(s) + dstsize);
+	else
+		return (ft_strlen(s) + dst_len);
+}

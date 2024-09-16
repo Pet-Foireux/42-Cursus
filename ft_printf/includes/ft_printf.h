a6d5af09db3901e5e8339ce1699be5a2db14726e
PetFoireux <pet-foireux@oo>
2024-09-16 23:41:24 +0200

    shrek

---
+++ ft_printf/includes/ft_printf.h
@@ -0,0 +1,20 @@
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include "../libft/libft.h"
+# include <stdarg.h>
+
+int		ft_putchar(int c);
+int		ft_printf(const char *str, ...);
+
+// size_t	ft_numlen(int value);
+// size_t	ft_ptrlen(uintptr_t value);
+// size_t	ft_hexlen(unsigned int value);
+
+// void	ft_printid(int value, int *counter);
+// void	ft_putptr(uintptr_t num);
+// void	ft_printp(unsigned long long value, int *counter);
+// void	ft_printu(unsigned int value, int *counter);
+// void	ft_puthex(unsigned int num, const char c);
+// void	ft_printhex(unsigned int value, const char c, int *counter);
+
+#endif

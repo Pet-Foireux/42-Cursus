b9bb4a5f564d68c1a1198caad8202ed7e1471424
Marius Papin <mpapin@c1r4p1.42lehavre.fr>
2024-10-15 17:24:11 +0200

    JaimelesChipsss

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,31 +6,31 @@
 /*   By: mpapin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 16:00:51 by mpapin            #+#    #+#             */
-/*   Updated: 2024/09/10 16:00:51 by mpapin           ###   ########.fr       */
+/*   Updated: 2024/10/15 17:23:58 by mpapin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *str, const char *to_find, size_t len)
 {
 	size_t	len_n;
 	size_t	i;
 	size_t	j;
 
-	if (!haystack || !needle)
+	if (!str || !to_find)
 		return (NULL);
-	if (ft_strlen(needle) == 0)
-		return ((char *) haystack);
-	len_n = ft_strlen(needle);
+	if (ft_strlen(to_find) == 0)
+		return ((char *) str);
+	len_n = ft_strlen(to_find);
 	i = 0;
-	while (haystack[i] && i < len)
+	while (str[i] && i < len)
 	{
 		j = 0;
-		while (haystack[i + j] && haystack[i + j] == needle[j] && i + j < len)
+		while (str[i + j] && str[i + j] == to_find[j] && i + j < len)
 			j++;
 		if (j == len_n)
-			return ((char *) haystack + i);
+			return ((char *) str + i);
 		i++;
 	}
 	return (NULL);
